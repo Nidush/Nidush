b0bf92a13add2c689586c1bb4756ad11a41d9212
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2025-12-21 17:58:18 +0000

    Navbar developed (icons and layout complete, logo pending)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,64 +1,80 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform, StyleSheet } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';//  Tenho que meter o Ionicons do MaterialIcons da Google
+import { Platform, Image } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: '#4A665E', 
+        tabBarActiveTintColor: '#4A665E',
         tabBarInactiveTintColor: '#9BA3A1',
         headerShown: false,
         tabBarStyle: {
-          backgroundColor: '#F1F4E9', 
+          backgroundColor: '#F1F4E9',
           borderTopWidth: 0,
           height: Platform.OS === 'ios' ? 90 : 70,
           paddingBottom: Platform.OS === 'ios' ? 30 : 10,
-          elevation: 0, 
+          elevation: 0,
         },
         tabBarLabelStyle: {
           fontFamily: 'Nunito-SemiBold',
           fontSize: 12,
         },
-      }}>
-      
+      }}
+    >
+      {/* Home alterar para o logo da Nidush*/}
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? "home" : "home-outline"} size={24} color={color} />
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home-outline" size={26} color={color} />
           ),
         }}
       />
 
+      {/* Activities */}
       <Tabs.Screen
         name="Activities"
         options={{
           title: 'Activities',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? "leaf" : "leaf-outline"} size={24} color={color} />
+          tabBarIcon: ({ color }) => (
+            <Image
+              source={require('../../assets/navbar/spa.png')} 
+              style={{ width: 26, height: 26, tintColor: color }}
+              resizeMode="contain"
+            />
           ),
         }}
       />
 
+      {/* Routines*/}
       <Tabs.Screen
         name="Routines"
         options={{
           title: 'Routines',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? "sunny" : "sunny-outline"} size={24} color={color} />
+          tabBarIcon: ({ color }) => (
+            <Image
+              source={require('../../assets/navbar/routine.png')} 
+              style={{ width: 26, height: 26, tintColor: color }}
+              resizeMode="contain"
+            />
           ),
         }}
       />
 
+      {/* Rooms*/}
       <Tabs.Screen
         name="Rooms"
         options={{
           title: 'Rooms',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? "grid" : "grid-outline"} size={24} color={color} />
+          tabBarIcon: ({ color }) => (
+            <Image
+              source={require('../../assets/navbar/auto_awesome_mosaic.png')} 
+              style={{ width: 26, height: 26, tintColor: color }}
+              resizeMode="contain"
+            />
           ),
         }}
       />
