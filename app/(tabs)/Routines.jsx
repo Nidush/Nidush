21bd06e1abdc5e3ef11572f4dc9b5afcfc010a1c
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2025-12-27 16:48:57 +0000

    Configurações iniciais do projeto e ajustes de layout

--- app/(tabs)/Routines.jsx
+++ app/(tabs)/Routines.jsx
@@ -1,13 +1,15 @@
-import { StyleSheet } from 'react-native';
-import { ThemedView } from '@/components/themed-view';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ThemedText } from '@/components/themed-text';
 
 export default function Routines() {
-  return <ThemedView style={styles.container} />;
+  return (
+    <SafeAreaView className="flex-1 bg-[#33470a]">
+      <View className="items-center py-4">
+        <ThemedText className="text-[24px] text-[#354F52] font-[Nunito_700Bold]">
+          Routines
+        </ThemedText>
+      </View>
+    </SafeAreaView>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'transparent', 
-  },
-});
