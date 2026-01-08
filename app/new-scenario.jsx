aed9379e77657c758acd0036dc5d6111ad20b721
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2026-01-08 15:55:03 +0000

    Criação de ficheiro new cenario

--- app/new-scenario.jsx
+++ app/new-scenario.jsx
@@ -1,1 +1,9 @@
-// new-scenario incio
+import { View, Text } from 'react-native';
+
+export default function NewScenarioScreen() {
+  return (
+    <View>
+      <Text>New Scenario Screen</Text>
+    </View>
+  );
+}
