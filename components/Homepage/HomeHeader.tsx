a326e20ccdd55192549bf38669a8285fdec3f62b
pedroteixeira04 <pfmt04@ua.pt>
2026-01-10 02:59:56 +0000

    Refactor homepage components: add HomeHeader and StateWidget; implement BaseCard with masked view; update font imports and styles

---
+++ components/Homepage/HomeHeader.tsx
@@ -0,0 +1,60 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { Link } from 'expo-router';
+import React from 'react';
+import { Image, Pressable, Text, View } from 'react-native';
+
+interface HomeHeaderProps {
+  userName: string;
+}
+
+export const HomeHeader = ({ userName }: HomeHeaderProps) => {
+  const getGreeting = () => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour >= 5 && currentHour < 12) {
+      return 'Good morning';
+    } else if (currentHour >= 12 && currentHour < 18) {
+      return 'Good afternoon';
+    } else if (currentHour >= 18 && currentHour < 22) {
+      return 'Good evening';
+    } else {
+      return 'Good night';
+    }
+  };
+
+  const greeting = getGreeting();
+
+  return (
+    <View className="flex-row justify-between items-center mb-6 mt-4">
+      <View>
+        <Text
+          style={{ fontFamily: 'Nunito_600SemiBold' }}
+          className="text-2xl text-[#354F52]"
+        >
+          {greeting},
+        </Text>
+        <Text
+          style={{ fontFamily: 'Nunito_700Bold' }}
+          className="text-4xl text-[#354F52]"
+        >
+          {userName}
+        </Text>
+      </View>
+
+      <View className="flex-row items-center">
+        <Pressable className="mr-4">
+          <MaterialIcons name="notifications-none" size={36} color="#548F53" />
+        </Pressable>
+
+        <Link href="/Profile" asChild>
+          <Pressable>
+            <Image
+              source={require('@/assets/avatars/profile.png')}
+              className="w-16 h-16 rounded-full"
+            />
+          </Pressable>
+        </Link>
+      </View>
+    </View>
+  );
+};
